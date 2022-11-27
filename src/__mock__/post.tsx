54ce26d75bce76b2eed175ce4300dae76547e558
Jacquiline Genato <jacquiline.genato@gmail.com>
2022-11-27 16:15:02 +0800

    change API

--- src/__mock__/post.tsx
+++ src/__mock__/post.tsx
@@ -1,17 +1,17 @@
 export function getPost(id: any) {
   return fetch(
-    `https://localhost:7136/Post/GetPostById?id=` + id
+    `https://bjclc-api.herokuapp.com/Post/GetPostById?id=` + id
   ).then((data) => data.json());
 }
 
 export function getPosts() {
-  return fetch('https://localhost:7136/Post/GetPosts').then((data) =>
+  return fetch('https://bjclc-api.herokuapp.com/Post/GetPosts').then((data) =>
     data.json()
   );
 }
 
 export function findPost(keyword: string) {
   return fetch(
-    'https://localhost:7136/Post/Search?keyword=' + keyword
+    'https://bjclc-api.herokuapp.com/Post/Search?keyword=' + keyword
   ).then((data) => data.json());
 }
